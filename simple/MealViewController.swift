0793f03e046ba4b3ca0f98cc2cfcb3ad68cd31af
kakkki <kakki4062@gmail.com>
2019-12-12 16:00:19 +0900

    push navigationでmeal listからDetailに遷移できるようになった

--- simple/MealViewController.swift
+++ simple/MealViewController.swift
@@ -30,6 +30,15 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         nameTextField.delegate = self
+        
+        // Set up views if editing an existing Meal.
+        if let meal = meal {
+            navigationItem.title = meal.name
+            nameTextField.text   = meal.name
+            photoImageView.image = meal.photo
+            ratingControl.rating = meal.rating
+        }
+        
         // Enable the Save button only if the text field has a valid Meal name.
         updateSaveButtonState()
     }
